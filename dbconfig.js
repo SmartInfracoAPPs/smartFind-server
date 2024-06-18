73be6c9eb8aa3eb328ab2990624e9fe18df2a2c7
jojo-koomson <koomson.lm@gmail.com>
2024-06-18 15:05:44 +0000

    mongoDB

--- dbconfig.js
+++ dbconfig.js
@@ -13,7 +13,7 @@ const dbconfig = async () => {
   // MongoDB connection
   try {
     // mongodb://localhost:27017/geoLTE
-    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.3yy055d.mongodb.net/${process.env.MONGODB_DATABASE}?ssl=true`, {
+    await mongoose.connect(`mongodb://root:y2IW91lqKRaZwtCXA0wzV6ZxQcIqvR2hdCdzVdTOByfbhjgyQ0c3r8Grlo307zax@10.247.5.115:27017/goeLTE`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
