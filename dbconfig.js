6f77c4aa31223a821f8dbc5aa5a7a250548e1d5c
iamdanieljojokoomson <53906917+jojo-koomson@users.noreply.github.com>
2024-06-26 16:38:34 +0000

    new34

--- dbconfig.js
+++ dbconfig.js
@@ -13,7 +13,7 @@ const dbconfig = async () => {
   // MongoDB connection
   try {
     // mongodb://localhost:27017/geoLTE
-    await mongoose.connect(`mongodb://root:y2IW91lqKRaZwtCXA0wzV6ZxQcIqvR2hdCdzVdTOByfbhjgyQ0c3r8Grlo307zax@10.247.5.115:5432/?directConnection=true`, {
+    await mongoose.connect(`mongodb://root:tFhh2hQwHwKiIB7pwFhNfbylsafgxcfWOF9Yd532vcq4juGZFez0sQ02pa7abSMb@197.253.124.146:5432/?directConnection=true`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
