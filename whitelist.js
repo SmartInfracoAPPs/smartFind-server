80f27108158c0e2be8f44d344f17c9b16509c69b
jojo-koomson <koomson.lm@gmail.com>
2024-06-26 14:36:00 +0000

    div

--- whitelist.js
+++ whitelist.js
@@ -1,17 +1,13 @@
 // whitelist.js
 const whitelist = [
-    'http://localhost:3000',
-    'https://www.google.com',
-    'https://smartfullapp.vercel.app',
-    'http://localhost:3500',
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://smartfind-front.vercel.app',
-    'https://maps.googleapis.com/maps/api/mapsjs',
-    'http://10.247.5.180:85',
-    'file://*',
-    'https://*' 
-  ];
-  
-  export default whitelist;
-  
+  'https://smartnetfind.197.253.124.146.sslip.io',
+  'https://smfind.10.247.5.116.nip.io',
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://www.google.com',
+  'https://maps.googleapis.com/maps/api/mapsjs',
+  'file://*',
+  'https://*',
+];
+
+export default whitelist;
